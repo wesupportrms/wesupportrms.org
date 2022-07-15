a031b35415798fd9ca039c4a055fdcf9585d01b9
liukeke <liukeke@diynova.com>
2022-07-15 15:10:33 +0800

    feat: html and css

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+
+export default function Footer() {
+  return (
+    <div className={'footer'}>
+        <p>Copyright © 2022 wesupportrms.org</p>
+        <p>This page is licensed under a Creative Commons Attribution-NoDerivatives 4.0 International License&nbsp; 
+        <a href='https://creativecommons.org/licenses/by-nd/4.0/'>http://creativecommons.org/licenses/by-nd/4.0/.</a></p>
+    </div>
+  )
+}
